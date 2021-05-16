db67e3f0d2e8276c0c002aa27cdc799abe3b25f9
Sribarathvajasarma <62195656+Sribarathvajasarma@users.noreply.github.com>
2021-05-16 09:32:07 +0530

    Add files via upload

--- volume.scala
+++ volume.scala
@@ -1,5 +1,6 @@
+import scala.math
 object volume extends App{
-  def findvol(r:Double):Double=4/3*math.Pi*r*r*r;
+  def findvol(r:Double):Double=4/3*math.Pi*math.pow(r,3);
   printf("%.2f",findvol(5));
   print("\n");
 }
