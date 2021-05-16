5444264eee349bef4a685a1cae621db8edc4f975
Sribarathvajasarma <62195656+Sribarathvajasarma@users.noreply.github.com>
2021-05-16 14:17:31 +0530

    Add files via upload

--- totalcost.scala
+++ totalcost.scala
@@ -2,7 +2,7 @@ object totalcost extends App{
   def totalcoverprice(a:Int)=a*24.95;
   def discount(b:Int)=b*24.95*0.4;
   def finalcoverprice(c:Int)= totalcoverprice(c)-discount(c)
-  def shippingcost(y:Int)=if(y<50){y*3.0} else{50*3.0+(y-50)*0.75};
+  def shippingcost(y:Int)=if(y<50){3.00} else{3.00+(y-50)*0.75};
   def findtotal(x:Int)=finalcoverprice(x)+shippingcost(x);
 
   printf("%.2f\n",findtotal(60));
